83a262a3a48de3418c9b2d3a83568df76b4afa71
malithjkd <malithjayawardhana@gmail.com>
2024-12-06 21:18:51 +0800

    created files send image to google cloud locaton

--- rpi-cam/main.py
+++ rpi-cam/main.py
@@ -2,36 +2,44 @@
 
 from capture_image import capture_image
 from check_for_human import check_for_human
-from send_file_to_storage import send_to_network_storage
+#from send_file_to_storage import send_to_network_storage, send_files_to_gs
 
 import datetime
 import time
 import os
 
-def capture_and_process():
+from google.cloud import storage
+
+
+def capture_and_process(bucket):
     '''Capturing image and identify pricens of people on the image, if there is any person save the image to network storage'''
     # capture image
-    filename = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".jpg"
-    full_filename = "/home/malithjkd/Documents/data/" + filename
-    capture_image(full_filename)
-    time.sleep(1)  # Add a delay to ensure the file is saved before processing
-    person_detected = check_for_human(full_filename)
+    file_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".jpg"
+    full_file_path = "/home/malithjkd/Documents/data/" + file_name
+    capture_image(full_file_path)
+    time.sleep(0.5)  # Add a delay to ensure the file is saved before processing
+    person_detected = check_for_human(full_file_path)
     if person_detected:
         # Send the file to network storage
         remote_host = "192.168.3.30"
         remote_user = "malithjkd"
         remote_password = "R&Dtest_mj"
         remote_directory = "/home/malithjkd/Documents/image_data/"
-        send_to_network_storage(full_filename, remote_host, remote_user, remote_password, remote_directory)
+        
+        #send_to_network_storage(full_filename, remote_host, remote_user, remote_password, remote_directory)
+        # send the file to google storage
+        blob = bucket.blob(file_name)
+        blob.upload_from_filename(full_file_path)
+
     else:
         #remove the file from local storage
-        os.remove(full_filename)
+        os.remove(full_file_path)
 
     # Add the filename to the log file
     with open("/home/malithjkd/Documents/data/log.txt", "a+") as log_file:
-        log_file.write(full_filename +"\t" + str(person_detected) + "\n")
+        log_file.write(full_file_path +"\t" + str(person_detected) + "\n")
         log_file.flush()
-    
+
     return person_detected
 
 
@@ -39,14 +47,19 @@ def main():
     '''run capture_and_process function every 5 seconds until timer finish'''
     time_now = datetime.datetime.now()
     #end_time = time(2024, 11, 22, 8, 30, 00)
-    end_time = time_now + datetime.timedelta(minutes=90)
+    end_time = time_now + datetime.timedelta(minutes=3)
     #end_time  = time_now + datetime.timedelta(days=10)
     person_detected = False
 
+    # send the file to google storage
+    client = storage.Client()
+    bucket_name = "malith_storage_basket"
+    bucket = client.get_bucket(bucket_name)
+
     while datetime.datetime.now() < end_time:
-        person_detected = capture_and_process()
+        person_detected = capture_and_process(bucket)
         if person_detected:
-            time.sleep(3)  # Adjust the sleep time as needed
+            time.sleep(30)  # Adjust the sleep time as needed
         else:
             time.sleep(6)
 
--- rpi-cam/send_file_to_storage.py
+++ rpi-cam/send_file_to_storage.py
@@ -3,6 +3,11 @@
 import paramiko
 from scp import SCPClient
 import os
+from google.cloud import storage
+from google.oauth2 import service_account
+
+#credentials = service_account.Credentials.from_service_account_file('/path/to/your/service-account-file.json')
+#storage_client = storage.Client(credentials=credentials)
 
 def send_to_network_storage(local_file_path, remote_host, remote_user, remote_password, remote_directory):
     try:
@@ -25,28 +30,61 @@ def send_to_network_storage(local_file_path, remote_host, remote_user, remote_pa
         # Close the SSH connection
         ssh.close()
 
-# Example usage
-#local_file_path = "/home/malithjkd/Documents/data/2024-11-22-07-23-17.jpg"
-base_dir = "/home/malithjkd/Documents/"
-remote_host = "192.168.3.30"
-remote_user = "malithjkd"
-remote_password = "R&Dtest_mj"
-remote_directory = "/home/malithjkd/Documents/image_data/"
 
-# send all the files in the log file to the network storage
-log_file_path = "/home/malithjkd/Documents/data/log.txt"
+def test_send_to_network_storage():
+    local_file_path = "/home/malithjkd/Documents/data/2024-11-22-07-23-17.jpg"
+    base_dir = "/home/malithjkd/Documents/"
+    remote_host = "192.168.3.30"
+    remote_user = "malithjkd"
+    remote_password = "R&Dtest_mj"
+    remote_directory = "/home/malithjkd/Documents/image_data/"
 
-with open(log_file_path, "r") as log_file:
-    for line in log_file:
-        local_file_path = line.strip()
-        local_file_path = base_dir + local_file_path
-        # check if the file exists
-        if not os.path.exists(local_file_path):
-            print(f"File {local_file_path} does not exist")
-        else:
-            print(f"Sending file: {local_file_path}")
-            send_to_network_storage(local_file_path, remote_host, remote_user, remote_password, remote_directory)
-            #deleate file from local storage
-            os.remove(local_file_path)
-    # Clear the log file after processing all entries
-    open(log_file_path, "w").close()
+    # send all the files in the log file to the network storage
+    log_file_path = "/home/malithjkd/Documents/data/log.txt"
+
+    with open(log_file_path, "r") as log_file:
+        for line in log_file:
+            local_file_path = line.strip()
+            local_file_path = base_dir + local_file_path
+            # check if the file exists
+            if not os.path.exists(local_file_path):
+                print(f"File {local_file_path} does not exist")
+            else:
+                print(f"Sending file: {local_file_path}")
+                send_to_network_storage(local_file_path, remote_host, remote_user, remote_password, remote_directory)
+                #deleate file from local storage
+                os.remove(local_file_path)
+        # Clear the log file after processing all entries
+        open(log_file_path, "w").close()
+
+
+
+def send_files_to_gs(bucket_name,file_name,full_file_path):
+    #print(local_file_path)
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(full_file_path)
+
+
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    # bucket_name = "your-bucket-name"
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name)
+
+    # Note: The call returns a response only when the iterator is consumed.
+    for blob in blobs:
+        print(blob.name)
+
+
+#full_file_path = "/home/malithjkd/Documents/data/2024-11-29-14-57-12.jpg"
+#bucket_name = "malith_storage_basket"
+#file_name = "image_from_rpi"
+
+#send_files_to_gs(full_file_path,bucket_name,file_name)
+
+#list_blobs(bucket_name)
